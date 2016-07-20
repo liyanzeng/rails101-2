a76d0fcd6c8ec0598a71c7ec9713ad760cdc3792
liyanzeng <yanzengli@gmail.com>
2016-07-20 21:15:32 +0800

    add edit and deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+    def edit
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+    end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -19,11 +24,25 @@ class PostsController < ApplicationController
     end
   end
 
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to group_path(@group), notice: "Update Success"
+end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "Post Deleted!"
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
